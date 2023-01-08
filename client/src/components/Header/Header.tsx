9c5d72561f80a8faff55f0c6b60f990153888ab3
mitchell adel <aamagebril@hhs.nl>
2023-01-08 14:37:05 +0100

    fixing header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -7,6 +7,7 @@ import translations from "../../consts/translations";
 import Dropdown from "react-bootstrap/Dropdown";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { Nav, NavItem } from "react-bootstrap";
 
 type Translation = {
   [key: string]: {
@@ -24,25 +25,25 @@ const Header: React.FC = () => {
     <div className="header_container">
       <Container fluid>
         <Row>
-          <Col align={"left"} xs={6} md={6} lg={6}>
-            <div>
+          <Nav>
+            <Col md={9}>
               {" "}
-              <Link to="/courses">
-                {" "}
-                <Button className="courses_nav_tab"> Courses</Button>
-              </Link>
-            </div>
-          </Col>
-          <Col align={"right"} xs={6} md={6} lg={6}>
-            <Button href="/signup" className="signUp">
+              <Nav.Link className="link" href="/courses">
+                {(translations as Translation)[lang].courses}
+              </Nav.Link>{" "}
+            </Col>
+            <Col>
               {" "}
-              {(translations as Translation)[lang].signUp}
-            </Button>{" "}
-            <Button href="/signin" className="login">
-              {" "}
-              {(translations as Translation)[lang].signIn}
-            </Button>
-          </Col>
+              <Nav.Link className="link" href="/signup">
+                {(translations as Translation)[lang].signUp}
+              </Nav.Link>
+            </Col>
+            <Col>
+              <Nav.Link className="link" href="/signin">
+                {(translations as Translation)[lang].signIn}
+              </Nav.Link>
+            </Col>
+          </Nav>
         </Row>
       </Container>
     </div>
