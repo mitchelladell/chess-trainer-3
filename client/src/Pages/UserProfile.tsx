1bf8274840b563eaf30298f94d689fb9b666b0e8
mitchell adel <aamagebril@hhs.nl>
2023-02-09 22:23:06 +0100

    profile pages

--- client/src/Pages/UserProfile.tsx
+++ client/src/Pages/UserProfile.tsx
@@ -12,15 +12,9 @@ import { MaxStreak } from "../pgns/icons/UserProfileIcons/MaxStreak";
 
 const UserProfile = () => {
   const [direction, setDirection] = useState<"row" | "row-reverse">("row");
+  const subScribtedCourses = [1, 2, 3, 4];
+  const coursesMade = [1, 2, 3, 4];
 
-  const items = [
-    { "تـاريخ الانضمام": 100 },
-    { "نقاط الخـبـرة المكتسبة": 90 },
-    { "تـاريخ الانضمام": 90 },
-    { "item 4": 90 },
-  ];
-
-  let coursesList = ["opening,", "endgame", "queen opening", "king opening"];
   const lang = useAppSelector((state: any) => state.language.value);
 
   useEffect(() => {
@@ -146,6 +140,59 @@ const UserProfile = () => {
                 ></div>
               </Col>
             </Row>
+          </div>
+          <div className="profile-intro-text" style={{ textAlign: "right" }}>
+            {" "}
+            الدورات التـي قمت بدراستها
+          </div>
+          <div
+            style={{
+              display: "inline-flex",
+              width: "100%",
+              overflowX: "auto",
+              justifyContent: "center",
+            }}
+          >
+            {subScribtedCourses.map((course) => (
+              <div
+                style={{
+                  backgroundImage: ``,
+                  background: "gold",
+                  height: "10vw",
+                  width: "15vw",
+                  minHeight: "100px",
+                  margin: "15px",
+                  minWidth: "100px",
+                }}
+              ></div>
+            ))}
+          </div>
+          <div className="profile-intro-text" style={{ textAlign: "right" }}>
+            {" "}
+            الدورات التـي قمت بإعدادها
+          </div>
+          <div
+            style={{
+              display: "inline-flex",
+              width: "100%",
+              overflowX: "auto",
+              justifyContent: "center",
+            }}
+          >
+            {subScribtedCourses.map((course) => (
+              <div
+                style={{
+                  backgroundImage: ``,
+                  background: "gold",
+                  backgroundRepeat: "no-repeat",
+                  height: "10vw",
+                  width: "15vw",
+                  margin: "15px",
+                  minHeight: "100px",
+                  minWidth: "100px",
+                }}
+              ></div>
+            ))}
           </div>
         </Container>
       </div>
