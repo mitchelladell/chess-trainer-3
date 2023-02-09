cc48d3478b0fa53b7e19819ea16d50d5a1dc690c
mitchell adel <aamagebril@hhs.nl>
2023-02-09 21:46:14 +0100

    more pages

--- client/src/Pages/UserProfile.tsx
+++ client/src/Pages/UserProfile.tsx
@@ -3,6 +3,12 @@ import { useAppSelector } from "../app/hooks";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./user_profile.css";
+import { StarSign } from "../pgns/icons/UserProfileIcons/StarSign";
+import { JoinDate } from "../pgns/icons/UserProfileIcons/JoinDate";
+import { WhomIFollow } from "../pgns/icons/UserProfileIcons/WhomIFollow";
+import { WhoFollowsMe } from "../pgns/icons/UserProfileIcons/whoFollowsMe";
+import { Streak } from "../pgns/icons/UserProfileIcons/Streak";
+import { MaxStreak } from "../pgns/icons/UserProfileIcons/MaxStreak";
 
 const UserProfile = () => {
   const [direction, setDirection] = useState<"row" | "row-reverse">("row");
@@ -18,7 +24,7 @@ const UserProfile = () => {
   const lang = useAppSelector((state: any) => state.language.value);
 
   useEffect(() => {
-    setDirection(lang === "en" ? "row" : "row-reverse");
+    setDirection(lang === "ar" ? "row" : "row-reverse");
     console.log("lang", lang);
   }, [lang]);
   return (
@@ -46,34 +52,96 @@ const UserProfile = () => {
         </Container>
         <Container>
           <div className="profile_container">
-            <Row>
-              <Col align={"end"} xs={12} sm={9} md={6} lg={6}>
-                {items.map((item) => (
+            <Row style={{ flexDirection: direction }}>
+              <Col align={"end"} xs={12} sm={9} md={9} lg={9}>
+                <div style={{ display: "inline-flex", width: "100%" }}>
                   <div className="item_dashboard">
-                    {Object.entries(item).map(([key, value]) => (
-                      <div
-                        className="item_style"
-                        style={{
-                          flexDirection: direction,
-                        }}
-                      >
-                        <div className="item_keyword">{`${key}`}</div>
-                        <div className="item_value">{`${value}`}</div>
-                      </div>
-                    ))}
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
                   </div>
-                ))}
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    {" "}
+                    <StarSign />
+                  </div>
+                </div>
+                <div style={{ display: "inline-flex", width: "100%" }}>
+                  <div className="item_dashboard">
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
+                  </div>
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    <JoinDate />
+                  </div>
+                </div>
+                <div style={{ display: "inline-flex", width: "100%" }}>
+                  <div className="item_dashboard">
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
+                  </div>
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    {" "}
+                    <WhomIFollow />
+                  </div>
+                </div>
+                <div style={{ display: "inline-flex", width: "100%" }}>
+                  <div className="item_dashboard">
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
+                  </div>
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    {" "}
+                    <WhoFollowsMe />
+                  </div>
+                </div>
+                <div style={{ display: "inline-flex", width: "100%" }}>
+                  <div className="item_dashboard">
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
+                  </div>
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    {" "}
+                    <Streak />
+                  </div>
+                </div>
+                <div style={{ display: "inline-flex", width: "100%" }}>
+                  <div className="item_dashboard">
+                    <div className="item_style">
+                      {" "}
+                      <div className="item_keyword">Hello</div>
+                      <div className="item_value">Hello</div>
+                    </div>
+                  </div>
+                  <div style={{ marginTop: "auto", marginBottom: "auto" }}>
+                    <MaxStreak />
+                  </div>
+                </div>
               </Col>
-              <Col align={"end"} xs={12} sm={9} md={6} lg={6}>
+              <Col align={"end"} xs={12} sm={3} md={3} lg={3}>
                 <div
                   style={{
                     backgroundImage: `url('../media/empty_profile_photo.png')`,
                     backgroundRepeat: "no-repeat",
-                    backgroundPosition: "right",
+                    backgroundPosition: "right top",
                     backgroundSize: "contain",
-                    height: "111px",
-                    width: "95%",
-                    marginTop: "15px",
+                    height: "100%",
+                    width: "100%",
+                    minHeight: "100px",
+                    minWidth: "100px",
                   }}
                 ></div>
               </Col>
