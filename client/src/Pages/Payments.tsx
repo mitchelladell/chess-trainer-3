5700575d1dcf4bb7c85c579f5a7762898bc9ebf8
mitchell adel <aamagebril@hhs.nl>
2023-02-23 11:37:08 +0100

    adding more theme

--- client/src/Pages/Payments.tsx
+++ client/src/Pages/Payments.tsx
@@ -1,36 +1,84 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { useAppSelector } from "../app/hooks";
 import "./Payments.css";
 
 const Payments = () => {
+  const theme = useAppSelector((state) => state.theme.value);
   return (
-    <div>
+    <div
+      style={{
+        background: theme === "dark" ? "" : "white",
+        height: "100%",
+      }}
+    >
       {" "}
       <Container>
         {" "}
         <Row>
           {" "}
           <div className="payments_keyword"> مشتـــريـــاتـــك</div>
-          <div className="payments_container">
+          <div
+            className="payments_container"
+            style={{
+              background:
+                theme === "dark"
+                  ? "#333333"
+                  : "linear-gradient(to left top, #808080, #ffffff)",
+            }}
+          >
             {" "}
             <Container>
               {" "}
               <Row>
                 {" "}
                 <Col>
-                  <div className="orange-texts">تاريخ الشـــراء </div>
+                  <div
+                    className="orange-texts"
+                    style={{
+                      color: theme === "dark" ? "" : "black",
+                    }}
+                  >
+                    تاريخ الشـــراء{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="orange-texts">صورة الدورة </div>
+                  <div
+                    className="orange-texts"
+                    style={{
+                      color: theme === "dark" ? "" : "black",
+                    }}
+                  >
+                    صورة الدورة{" "}
+                  </div>
                 </Col>
                 <Col>
                   {" "}
-                  <div className="orange-texts">مؤلف الدورة </div>
+                  <div
+                    className="orange-texts"
+                    style={{
+                      color: theme === "dark" ? "" : "black",
+                    }}
+                  >
+                    مؤلف الدورة{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="orange-texts">تكلفة الدورة </div>
+                  <div
+                    className="orange-texts"
+                    style={{
+                      color: theme === "dark" ? "" : "black",
+                    }}
+                  >
+                    تكلفة الدورة{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="orange-texts">
+                  <div
+                    className="orange-texts"
+                    style={{
+                      color: theme === "dark" ? "" : "black",
+                    }}
+                  >
                     {" "}
                     البـريد الإلكتـرونـي للمشتــرى
                   </div>
@@ -40,7 +88,8 @@ const Payments = () => {
                 style={{
                   background: "white",
                   height: "3px",
-                  border: "3px solid white",
+                  border: `3px solid ${theme === "dark" ? "white" : "black"}`,
+
                   width: "80%",
                   margin: "auto",
                 }}
@@ -49,28 +98,57 @@ const Payments = () => {
               </div>
               <Row>
                 <Col>
-                  <div className="white-texts">2022/4/13 </div>
+                  <div
+                    className="white-texts"
+                    style={{
+                      color: theme === "light" ? "black" : "white",
+                    }}
+                  >
+                    2022/4/13{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="white-texts">
+                  <div>
                     <img width={"75px"} height="75px" alt="" />{" "}
                   </div>
                 </Col>
                 <Col>
                   {" "}
-                  <div className="white-texts">اسم مؤلف الدورة التدريبية </div>
+                  <div
+                    className="white-texts"
+                    style={{
+                      color: theme === "light" ? "black" : "white",
+                    }}
+                  >
+                    اسم مؤلف الدورة التدريبية{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="white-texts"> 1200 EP </div>
+                  <div
+                    className="white-texts"
+                    style={{
+                      color: theme === "light" ? "black" : "white",
+                    }}
+                  >
+                    {" "}
+                    1200 EP{" "}
+                  </div>
                 </Col>
                 <Col>
-                  <div className="white-texts"> lolasasa123@gmail.com </div>
+                  <div
+                    className="white-texts"
+                    style={{
+                      color: theme === "light" ? "black" : "white",
+                    }}
+                  >
+                    {" "}
+                    lolasasa123@gmail.com{" "}
+                  </div>
                 </Col>
                 <div
                   style={{
-                    background: "white",
                     height: "3px",
-                    border: "3px solid white",
+                    border: `3px solid ${theme === "dark" ? "white" : "black"}`,
                     width: "80%",
                     margin: "auto",
                   }}
--- client/src/Pages/WishList.tsx
+++ client/src/Pages/WishList.tsx
@@ -1,28 +1,44 @@
 import { Container, Row, Col } from "react-bootstrap";
 import CourseCard from "../components/CourseCard/CourseCard";
+import { useAppSelector } from "../app/hooks";
 import "./WishList.css";
 const WishList = () => {
+  const theme = useAppSelector((state) => state.theme.value);
   return (
-    <Container>
-      <Row>
-        <Col md={2} lg={2} sm={6} xs={6}>
-          {" "}
-          <div className="ads-area"> Hello</div>
-        </Col>{" "}
-        <Col md={12} lg={10} sm={6} xs={6}>
-          <div className="wish_list_container">
+    <div
+      style={{
+        background: theme === "dark" ? "" : "white",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col md={2} lg={2} sm={6} xs={6}>
             {" "}
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-            <CourseCard buy={true} />
-          </div>{" "}
-        </Col>
-      </Row>
-    </Container>
+            <div className="ads-area"> Hello</div>
+          </Col>{" "}
+          <Col md={12} lg={10} sm={6} xs={6}>
+            <div
+              className="wish_list_container"
+              style={{
+                background:
+                  theme === "dark"
+                    ? "#333333"
+                    : "linear-gradient(to left top, #808080, #ffffff)",
+              }}
+            >
+              {" "}
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+              <CourseCard buy={true} />
+            </div>{" "}
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
